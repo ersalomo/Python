5552beb00416168c946ae32dbd8c700ff4f6cf67
ersalomo <95175703+ersalomo@users.noreply.github.com>
2022-03-26 19:06:38 +0700

    menambah class baru

--- oop.py
+++ oop.py
@@ -1,4 +1,7 @@
 
+from types import ClassMethodDescriptorType
+
+
 class Karyawan:
     nama_perusahaan = 'PT. Bersama Kita Hebat'
     insentif_lembur = 250000
@@ -10,15 +13,19 @@ class Karyawan:
         print(self.display())
 
     def display(self):
-        return 'Nama Karyawan : {}\nUsia\t  :{}\nGaji\t  :{}'.format(self.__nama, self.__usia, self.__gaji)
+        return 'Nama Karyawan : {}\nUsia   \t  :{}\nGaji   \t  :{}'.format(self.__nama, self.__usia, self.__gaji)
 
     def lembur(self):
         self.__gaji +=self.insentif_lembur
 
     def tambahan_proyek(self, nilai_proyek):
         self.pendapatan_tambahan +=nilai_proyek
+
     def getGaji(self):
         return self.__gaji
+
+    def getName(self):
+        return self.__nama
 
     def tot_pendapatan(self):
         return self.__gaji + self.pendapatan_tambahan
@@ -48,23 +55,63 @@ class Admin(Karyawan):
     def __init__(self,**kwargs):
         # super().__init__()jika kosong maka data Class Admin  None
         super().__init__(**kwargs)
-        self.__nama =kwargs.get('nama')
-        self.__usia = kwargs.get('usia')
-        self.__gaji = kwargs.get('gaji')
+        # tidak terpakai
+        # self.__nama =kwargs.get('nama')
+        # self.__usia = kwargs.get('usia')
+        # self.__gaji = kwargs.get('gaji')
         self.__hobi = kwargs.get('hobi')
         print(self.getHobi())
     def getHobi(self):
-        return 'Hobbi\t:{}\n'.format(self.__hobi)
+        return 'Occupation\t:{}\n'.format(self.__hobi)
+
+class Dokter(Karyawan):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__specialis = kwargs.get('specialis')
+        print(self.getJob())
+
+    def getJob(self):
+        return 'Hobbi\t:{}\n'.format(self.__specialis) 
+    
+    def tambahan_proyek(self, value_of_proyek):
+        self.pendapatan_tambahan +=value_of_proyek*0.2
+
+    
+
+class salesAdmin(Admin):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+
+    # kita harus mendefinisikan staticmethod agar dapat mengakses kelas parent
+    @classmethod
+    def getLocation(cl):
+        return cl.nama_perusahaan
+
+    @classmethod
+    def coba(cls):
+        return cls.insentif_lembur
+
 
 jonas = Karyawan(nama="Jonas",usia=21,gaji=600000)
 print()
 ersalomo = Admin(nama="Ersalomo", usia=20, gaji=600000,hobi='administrator')
 agung = Admin(nama="Agung", usia=20, gaji=100000,hobi='administrator')
 
+selena = Dokter(nama="Selena", usia=22, gaji=10000000,specialis="kandungan")
+yakamura = salesAdmin(nama="yakamura", usia=19, gaji=12000000)
+
 pt_makmur = Perusahaan("PT Abadi",'Jl. Merdeka lalap 29','+20920118') 
-
-
 pt_makmur.tambahan_karyawan(jonas)
 pt_makmur.tambahan_karyawan(ersalomo)
 pt_makmur.tambahan_karyawan(agung)
-print(pt_makmur.getKaryawan())
+
+print('')
+selena.lembur()
+selena.tambahan_proyek(100000)
+print(f'Total gaji {jonas.getName()} : {jonas.tot_pendapatan()}')
+print(f'Total gaji {ersalomo.getName()} : {ersalomo.tot_pendapatan()}')
+print(f'Total gaji {agung.getName()} : {agung.tot_pendapatan()}')
+print(f'Total gaji {selena.getName()} : {selena.tot_pendapatan()}')
+
+print(f'bio data {yakamura.getName()} bekerja di {yakamura.getLocation()}')
+print(yakamura.coba())
